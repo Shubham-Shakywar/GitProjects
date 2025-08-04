3fea6da15fd8929e3614b474bf37847cac28f947
Meetanshi Satpute <meetanshisatpute@gmail.com>
2025-08-04 20:26:37 +0530

    no need extra

---
+++ noneed.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const NotNeed = () => {
+  return (
+    <View>
+      <Text>NotNeed</Text>
+    </View>
+  )
+}
+
+export default NotNeed
+
+const styles = StyleSheet.create({})
